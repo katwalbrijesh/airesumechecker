d7b98352630aaa61dfa87d35c74c0e00d81a5062
Brijesh <katwalbrijesh14@gmail.com>
2026-06-26 19:03:30 +0545

    first commit

---
+++ frontend/AiResumeChecker/src/lib/utils.js
@@ -0,0 +1,23 @@
+export const cn = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};
+
+export function relativeTime(date) {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  const intervals = {
+    year: 31536000,
+    month: 2592000,
+    week: 604800,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+  };
+
+  for (const [key, value] of Object.entries(intervals)) {
+    const interval = Math.floor(seconds / value);
+    if (interval >= 1) {
+      return `${interval} ${key}${interval > 1 ? 's' : ''} ago`;
+    }
+  }
+  return 'just now';
+}
